dd05b11799775e94426c546511e6f3f9a85b94b7
RakibulIslam1161062 <bsse0903@iit.du.ac.bd>
2022-06-16 19:22:11 +0000

    movie ui complete

--- frontend/src/Movie/MovieCard.tsx
+++ frontend/src/Movie/MovieCard.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+// import {FcLikePlaceholder} from 'react-icons/fc'
 
 export default function MovieCard() {
     return (
-        <div className="card" style={{width: "18rem"}}>
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+        //     <div className="card" style={{width: "18rem"}}>
+        //     <img className="card-img-top" src="https://www.qries.com/images/banner_logo.png" alt="Card image cap" />
+        //     <div className="card-body">
+        //       <h5 className="card-title">Card title</h5>
+        //       <p className="card-text">d's content.</p>
+        //       <a href="#" className="btn btn-primary">Go somewhere</a>
+        //     </div>
+        //   </div>
+
+        <div className="container">
+            {/* <span><FcLikePlaceholder fontSize='1.5rem' /></span> */}
+            <img src="https://picsum.photos/200" alt="poster" />
+            <p>hi</p>
         </div>
-      </div>
-      );
+    );
 }
--- frontend/src/Movie/MovieList.tsx
+++ frontend/src/Movie/MovieList.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import MovieCard from './MovieCard'
+import MovieContainer from './MovieContainer'
 
 export default function MovieList() {
   return (
-   <MovieCard />
+    <MovieContainer />
   )
 }
